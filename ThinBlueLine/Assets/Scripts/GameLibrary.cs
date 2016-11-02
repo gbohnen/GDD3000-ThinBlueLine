fbfbe04f814a150263990d15bec3589a301f8a4b
gbohnen <fezmonkey@live.com>
2016-11-01 23:51:58 -0600

    Basic player select backend

---
+++ ThinBlueLine/Assets/Scripts/GameLibrary.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum Players { Player1, Player2, Player3, Player4 }
+
+public struct Player
+{
+    public string name;
+    public string desc;
+    public int muscle;
+    public int smarts;
+    public int moxie;
+    public string special;
+    public string story;
+}
+
+public class GameLibrary : MonoBehaviour {
+
+    private static GameLibrary instance;
+
+    private List<Player> avatars;
+    //private List<Situation> situationDeck;
+    //private List<MajorCrimes> crimeDeck;
+    // etc.
+
+    private GameLibrary()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    public void Initialize()
+    {
+        avatars = LoadGameData.LoadPlayers();
+        //situationDeck = LoadGameData.LoadSituations();
+        //Shuffle(situationDeck);
+        //crimeDeck = LoadGameData.LoadCrimes();
+        //Shuffle(crimeDeck);
+    }
+
+    public GameLibrary Instance
+    {
+        get
+        {
+            if (instance = null)
+                instance = new GameLibrary();
+
+            return instance;
+        }
+    }
+
+    public List<Player> Avatars
+    {
+        get { return avatars; }
+    }
+}
---
+++ ThinBlueLine/Assets/Scripts/XML Tools/LoadGameData.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml;
+using System;
+using System.Collections.Generic;
+
+
+public static class LoadGameData {
+
+    public static TextAsset playerFile;
+
+    public static List<Player> LoadPlayers()
+    {
+        // instantiate necessary components
+        XmlDocument playerDoc = new XmlDocument();                                      // blank xml doc
+        playerDoc.LoadXml(playerFile.text);                                             // load player file
+        XmlNodeList avatarList = playerDoc.GetElementsByTagName("avatar");              // get all tags labeled avatar
+
+        List<Player> players = new List<Player>();                                      // list of player objects
+
+        // iterate all "avatar" tags
+        foreach (XmlNode node in avatarList)                                            
+        {
+            // make a list of all child node (this is where the palyer data is stored)
+            XmlNodeList playerData = node.ChildNodes;                                           
+
+            // empty player
+            Player player = new Player();
+
+            // iterate those nodes
+            foreach (XmlNode childNode in playerData)
+            {
+                // load data into player based on child tag
+                switch (childNode.Name)
+                {
+                    case "name":    player.name = childNode.InnerText; break;
+                    case "desc":    player.desc = childNode.InnerText; break;
+                    case "muscle":  player.muscle = Int32.Parse(childNode.InnerText); break;
+                    case "smarts":  player.smarts = Int32.Parse(childNode.InnerText); break;
+                    case "moxie":   player.moxie = Int32.Parse(childNode.InnerText); break;
+                    case "special": player.special = childNode.InnerText; break;
+                    case "story":   player.story = childNode.InnerText; break;
+                    default:        break;
+                }
+            }
+
+            // add child to the list
+            players.Add(player);
+        }
+
+        // return list of all players
+        return players;
+    }
+}
