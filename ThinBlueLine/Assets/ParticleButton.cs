c62abb422b8d9defd9173db9b6913e18d722657b
gbohnen <fezmonkey@live.com>
2016-11-04 15:50:22 -0600

    Particle button test

---
+++ ThinBlueLine/Assets/ParticleButton.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ParticleButton : MonoBehaviour {
+
+    ParticleSystem emitter;
+
+    void Start()
+    {
+        emitter = this.GetComponentInChildren<ParticleSystem>();
+
+        
+        emitter.startColor = Color.blue;
+            
+        
+            //this.GetComponent<Image>().color;
+    }
+
+    public void OnMouseEnter()
+    {
+        emitter.Play();
+    }
+
+     public void OnMouseExit()
+    {
+        emitter.Stop();
+    }
+
+
+}
