7956afa613305f8b03673a9cde89c9cd35fb0075
gbohnen <fezmonkey@live.com>
2016-11-05 18:38:12 -0600

    Fixed broken DLLs

--- ThinBlueLine/Assets/Scripts/GameLibrary.cs
+++ ThinBlueLine/Assets/Scripts/GameLibrary.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using System.Collections;
 using System.Collections.Generic;
 using Assets.Scripts;
 
@@ -57,7 +56,7 @@ public class GameLibrary : MonoBehaviour {
     {
         get
         {
-            if (instance = null)
+            if (instance == null)
                 instance = new GameLibrary();
 
             return instance;
