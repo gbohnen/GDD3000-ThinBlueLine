55d67209740c9603ac321546866e37944ac8e003
KatlynM <katlynmayo7@gmail.com>
2016-11-06 13:56:01 -0700

    Comment

--- ThinBlueLine/Assets/Scripts/Cards/PlayerScript.cs
+++ ThinBlueLine/Assets/Scripts/Cards/PlayerScript.cs
@@ -32,7 +32,7 @@ namespace Assets.Scripts
         }
 
         /// <summary>
-        /// Gets or sets the players strength
+        /// Gets the players strength
         /// </summary>
         public int Strength
         {
