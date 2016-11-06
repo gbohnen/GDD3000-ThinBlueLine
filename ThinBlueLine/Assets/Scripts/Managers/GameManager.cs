e931ab9f727b48a8db579d70751158d94a02b50b
KatlynM <katlynmayo7@gmail.com>
2016-11-05 20:17:41 -0600

    Added the fields to the Gamemanager, added UpdateUI method, connected the text in the inspector

--- ThinBlueLine/Assets/Scripts/Managers/GameManager.cs
+++ ThinBlueLine/Assets/Scripts/Managers/GameManager.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 /// <summary>
 /// Singleton class which manages the state of the game
 /// </summary>
-public class GameManager
+public class GameManager : MonoBehaviour
 {
     #region Fields
 
@@ -40,6 +40,27 @@ public class GameManager
     // store the mob boss & police chief
     MobBossScript mobBoss;
     PoliceChiefScript policeChief;
+
+    // player UI fields
+    public Text playerWName;
+    public Text playerWSma;
+    public Text playerWMox;
+    public Text playerWMus;
+
+    public Text playerXName;
+    public Text playerXSma;
+    public Text playerXMox;
+    public Text playerXMus;
+
+    public Text playerYName;
+    public Text playerYSma;
+    public Text playerYMox;
+    public Text playerYMus;
+
+    public Text playerZName;
+    public Text playerZSma;
+    public Text playerZMox;
+    public Text playerZMus;
 
     #endregion
 
@@ -81,6 +102,44 @@ public class GameManager
     /// </summary>
     public AudioManager Audio
     { get; private set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    void Start()
+    {
+        UpdateUI();
+    }
+
+    /// <summary>
+    /// Updates the UI
+    /// </summary>
+    public void UpdateUI()
+    {
+        // player one
+        playerWName.text = GameLibrary.Instance.Players[Players.Player1].Name;
+        playerWSma.text = GameLibrary.Instance.Players[Players.Player1].Smarts.ToString();
+        playerWMox.text = GameLibrary.Instance.Players[Players.Player1].Moxie.ToString();
+        playerWMus.text = GameLibrary.Instance.Players[Players.Player1].Strength.ToString();
+
+        // player two
+        playerXName.text = GameLibrary.Instance.Players[Players.Player2].Name;
+        playerXSma.text = GameLibrary.Instance.Players[Players.Player2].Smarts.ToString();
+        playerXMox.text = GameLibrary.Instance.Players[Players.Player2].Moxie.ToString();
+        playerXMus.text = GameLibrary.Instance.Players[Players.Player2].Strength.ToString();
+
+        // player three
+        playerYName.text = GameLibrary.Instance.Players[Players.Player3].Name;
+        playerYSma.text = GameLibrary.Instance.Players[Players.Player3].Smarts.ToString();
+        playerYMox.text = GameLibrary.Instance.Players[Players.Player3].Moxie.ToString();
+        playerYMus.text = GameLibrary.Instance.Players[Players.Player3].Strength.ToString();
+
+        // player four
+        playerZName.text = GameLibrary.Instance.Players[Players.Player4].Name;
+        playerZSma.text = GameLibrary.Instance.Players[Players.Player4].Smarts.ToString();
+        playerZMox.text = GameLibrary.Instance.Players[Players.Player4].Moxie.ToString();
+        playerZMus.text = GameLibrary.Instance.Players[Players.Player4].Strength.ToString();
+    }
 
     /// <summary>
     /// Passes the current turn to the next player
