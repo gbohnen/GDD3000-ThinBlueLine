bff75570a4a7b3a44de5863a56800e72f0801f04
KatlynM <katlynmayo7@gmail.com>
2016-11-06 23:10:29 -0700

    Created two versions of the prefab for situation cards, and a script to go with it

---
+++ ThinBlueLine/Assets/Scripts/SituationSpawnScript.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Assets.Scripts;
+
+/// <summary>
+/// Handles the situations
+/// </summary>
+public class SituationSpawnScript : MonoBehaviour
+{
+    #region Fields
+
+    // store the prefab object
+    public GameObject situationCardPrefab;
+
+    // store the situations information
+    public Text situationName;
+    public Text cost;
+    //public Text smartsCost;
+    //public Text moxieCost;
+    //public Text strengthCost;
+    public Text description;
+
+    // store the demo situation
+    SituationScript demoSitch;
+
+    #endregion
+
+    #region Public Methods
+
+    // Use this for initilization
+    void Start()
+    {
+        demoSitch = new SituationScript(LoadGameData.LoadSituations()[0]);
+        UpdateFields();
+    }
+
+    /// <summary>
+    /// Updates the information for the situation card
+    /// </summary>
+    public void UpdateFields()
+    {
+        situationName.text = demoSitch.Name;
+        cost.text = demoSitch.Cost.ToString();
+        description.text = demoSitch.Description;
+    }
+
+    /// <summary>
+    /// Closes the window
+    /// </summary>
+    public void Close()
+    { Destroy(this); }
+
+    #endregion
+}
