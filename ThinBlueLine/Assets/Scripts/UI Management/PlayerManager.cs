b26298fcabecb97ef9c5a0cb821064c96ba444c9
gbohnen <fezmonkey@live.com>
2016-12-11 14:11:58 -0700

    Lots of garbage

--- ThinBlueLine/Assets/Scripts/UI Management/PlayerManager.cs
+++ ThinBlueLine/Assets/Scripts/UI Management/PlayerManager.cs
@@ -28,6 +28,16 @@ public class PlayerManager : MonoBehaviour {
     public Text playerZMox;
     public Text playerZMus;
 
+    public Image playerWAvat;
+    public Image playerXAvat;
+    public Image playerYAvat;
+    public Image playerZAvat;
+
+    public Image playerWindic;
+    public Image playerXindic;
+    public Image playerYindic;
+    public Image playerZindic;
+
     // store the animation indicator
     public Animator indicatorAnimator;
 
@@ -40,11 +50,17 @@ public class PlayerManager : MonoBehaviour {
         playerWMox.text = GameLibrary.instance.Players[Players.Player1].Moxie.ToString();
         playerWMus.text = GameLibrary.instance.Players[Players.Player1].Muscle.ToString();
 
+        playerWAvat.sprite = GameLibrary.instance.Players[Players.Player1].Avatar;
+        playerWindic.sprite = GameLibrary.instance.Players[Players.Player1].Avatar;
+
         // player two
         playerXName.text = GameLibrary.instance.Players[Players.Player2].Name;
         playerXSma.text = GameLibrary.instance.Players[Players.Player2].Smarts.ToString();
         playerXMox.text = GameLibrary.instance.Players[Players.Player2].Moxie.ToString();
         playerXMus.text = GameLibrary.instance.Players[Players.Player2].Muscle.ToString();
+
+        playerXAvat.sprite = GameLibrary.instance.Players[Players.Player2].Avatar;
+        playerXindic.sprite = GameLibrary.instance.Players[Players.Player2].Avatar;
 
         // player three
         playerYName.text = GameLibrary.instance.Players[Players.Player3].Name;
@@ -52,11 +68,17 @@ public class PlayerManager : MonoBehaviour {
         playerYMox.text = GameLibrary.instance.Players[Players.Player3].Moxie.ToString();
         playerYMus.text = GameLibrary.instance.Players[Players.Player3].Muscle.ToString();
 
+        playerYAvat.sprite = GameLibrary.instance.Players[Players.Player3].Avatar;
+        playerYindic.sprite = GameLibrary.instance.Players[Players.Player3].Avatar;
+
         // player four
         playerZName.text = GameLibrary.instance.Players[Players.Player4].Name;
         playerZSma.text = GameLibrary.instance.Players[Players.Player4].Smarts.ToString();
         playerZMox.text = GameLibrary.instance.Players[Players.Player4].Moxie.ToString();
         playerZMus.text = GameLibrary.instance.Players[Players.Player4].Muscle.ToString();
+
+        playerZAvat.sprite = GameLibrary.instance.Players[Players.Player4].Avatar;
+        playerZindic.sprite = GameLibrary.instance.Players[Players.Player4].Avatar;
 
         // update current player avatar
         avatThumb.sprite = GameLibrary.instance.Players[GameManager.Instance.CurrentPlayer].Avatar;
